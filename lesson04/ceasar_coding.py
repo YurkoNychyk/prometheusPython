1ab49f5187ac952858d3d8c6008fea880ab64660
Yurko Nychyk <yurko.nychik@gmail.com@>
2022-03-18 18:09:44 +0200

    Ceasar coding: added interaction menu with encode decode and quit options

--- lesson04/ceasar_coding.py
+++ lesson04/ceasar_coding.py
@@ -1,22 +1,59 @@
 #Coding/Encoding strings with Ceasar code
 import sys
-#input_message = str(raw_input('Enter the message to encode:'))
-#encoding_shift = str(raw_input('Enter code shift:'))
-input_message = str(sys.argv[1])
-encoding_shift = int(sys.argv[2])
-encoded_message = []
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-print "Input string: " + input_message
-print "Encoding shift: " + str(encoding_shift)
-alphabet_index = 0
-for letter in input_message:
-    print "Letter " + letter
-    alphabet_index = 0
-    while alphabet_index < len(alphabet):
-        if letter == alphabet[alphabet_index]:
-            print alphabet[alphabet_index + encoding_shift]
-            encoded_message.append(alphabet[alphabet_index + encoding_shift])
-            break
-        alphabet_index = alphabet_index + 1
+
+encode_decode_answer = " "
+
+
+while encode_decode_answer != 'e' and encode_decode_answer != 'd' and encode_decode_answer !='q':
     
-print encoded_message
+    print " "
+    encode_decode_answer = str(raw_input ('Dou you want to Encode(e) or Decode(d) your message? If neither -- press (q) '))     
+
+    if encode_decode_answer != 'q':
+
+        output_message = ''
+
+        input_message = str(raw_input('Enter your message:'))
+        encoding_shift = int(raw_input('Enter code shift:'))    
+        print ""
+    
+        alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+        for letter in input_message:
+    
+            current_letter_index = alphabet.find (letter)
+            if current_letter_index != -1:
+           
+                if encode_decode_answer == 'e':
+                    
+                    print 'Encoding...'
+                    
+                    if current_letter_index + encoding_shift < len(alphabet):
+                        
+                        output_message = output_message + alphabet[current_letter_index + encoding_shift]
+                        
+                    else:
+                        
+                        output_message = output_message + alphabet[current_letter_index + encoding_shift - len(alphabet)]
+
+
+                elif encode_decode_answer == 'd':
+                    print 'Decoding...'
+                    output_message = output_message + alphabet[current_letter_index - encoding_shift]
+     
+            else:
+    
+                output_message = output_message + letter    
+        
+        print ""
+        print "Your message:"
+        print output_message
+        encode_decode_answer = " "
+
+    else:
+        print ""
+        print 'Quitting'
+        break
+
+    
+    
