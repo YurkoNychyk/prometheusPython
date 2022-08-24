cb85e2d71553e5a8c2f6073c865dbc97588f515e
Yurko Nychyk <yurko.nychik@gmail.com@>
2022-08-24 18:22:44 +0300

    Lesson08: task 8.4 new method

--- Lesson08/sudoku.py
+++ Lesson08/sudoku.py
@@ -1,22 +1,3 @@
 def make_sudoku(size):
     import random
     print "SUDOKU"
-    matrix = [[]]
-    print matrix[0]
-    for r in range(0, size*size):
-        matrix.append([])
-        while len(matrix[r]) < size*size:
-            n = random.randint(1, size*size)
-            #print "matrix %d row is %d of length" % (r, len(matrix[r]))
-
-            if n not in matrix[r]:
-                if r > 0:
-                    if n != matrix[r-1][len(matrix[r])]:
-                        #print "add %d in %s" % (n, str(matrix[r]))
-                        matrix[r].append(n)
-                else:
-                    #print "add %d in %s" % (n, str(matrix[r]))
-                    matrix[r].append(n)
-            print n
-            print matrix
-        print str(matrix[r]) + " " + str (r)
