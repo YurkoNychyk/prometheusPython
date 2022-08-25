d963b810ac6d156a5f85bc3ad9595bed2072d8af
Yurko Nychyk <yurko.nychik@gmail.com@>
2022-08-25 17:06:34 +0300

    Lesson08: task 8.4 complete on host. still checking on Prometheus

--- Lesson08/lesson08.py
+++ Lesson08/lesson08.py
@@ -1,7 +1,7 @@
 from combStr import *
 from FindFraction import *
 from bouquets import *
-
+from sudoku import *
 s0 = CombStr('qqqqqqweqqq%')
 
 print s0.count_substrings(0) # 0
@@ -15,8 +15,6 @@ print find_fraction(2) # (1, 2)
 
 print ("Bouquets")
 print bouquets(1,1,1,5) # 34
-print bouquets(2,3,4,10) # 12
-print bouquets(2,3,4,100) # 4019
-print bouquets(200,300,400,10000) # 4019
-print bouquets(200,300,400,100000) # 3524556
-print bouquets(15.5,4.1,5.99,21.75)
+
+print make_sudoku(3)
+
--- Lesson08/sudoku.py
+++ Lesson08/sudoku.py
@@ -1,3 +1,19 @@
 def make_sudoku(size):
     import random
-    print "SUDOKU"
+    #print "SUDOKU"
+    random_row = random.sample(range(1, size*size+1), size*size)
+    #random_row = range(1,size*size+1)
+    matrix = [random_row]
+    #print matrix[0]
+    for row in range(1, size*size):
+        if row % size == 0 and row != 1:
+            shift = size + 1
+        else:
+            shift = size
+
+        matrix.append(matrix[row-1][shift:] + matrix[row-1][:shift])
+
+    return matrix
+
+
+
